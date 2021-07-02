bffbe10f8819661dae9e604ca2c4543363925a9f
debug-anywhere001 <i@debug-anywhere.com>
2021-07-02 22:59:45 +0800

    chore: ajax-intercept

--- ajax-intercept/README.md
+++ ajax-intercept/README.md
@@ -8,13 +8,13 @@ intercept XHR and modify response.
 // intercept demo
 intercept_ajax([
     {
-		method: 'get',
-		pattern: /app\.json/gi,
-		callback: function(response){
-			var result = JSON.stringify({ name: "Debug Anywhere", version: "1.0" });
-			return result;
-		}
-	}
+        method: 'get',
+        pattern: /app\.json/gi,
+        callback: function(response){
+            var result = JSON.stringify({ name: "Debug Anywhere", version: "1.0" });
+            return result;
+        }
+    }
 ]);
 
 ```
--- ajax-intercept/index.js
+++ ajax-intercept/index.js
@@ -12,36 +12,36 @@ var cacheRules = [];
  * @param {Function} rules.callback  
  */
 function intercept_ajax (rules) {
-	cacheRules = cacheRules.concat(rules);
-	XMLHttpRequest.prototype.open = function() {
-		var open_arguments = arguments;
-		var method = open_arguments[0].toLowerCase(),
-			url = open_arguments[1];
+    cacheRules = cacheRules.concat(rules);
+    XMLHttpRequest.prototype.open = function() {
+        var open_arguments = arguments;
+        var method = open_arguments[0].toLowerCase(),
+            url = open_arguments[1];
 
-		// intercept or not
-		var flag = false;
-		var callback = function () {}; 
-		for(let i = 0, len = cacheRules.length; i < len; i++) {
-			var curRule = cacheRules[i];
-			if ((curRule.method ? curRule.method === method : true) 
-				&& curRule.pattern instanceof RegExp ? curRule.pattern.test(url) : curRule.pattern === url) {
-				flag = true;
-				callback = curRule.callback;
-				break;
-			}
-		}
+        // intercept or not
+        var flag = false;
+        var callback = function () {}; 
+        for(let i = 0, len = cacheRules.length; i < len; i++) {
+            var curRule = cacheRules[i];
+            if ((curRule.method ? curRule.method === method : true) 
+                && curRule.pattern instanceof RegExp ? curRule.pattern.test(url) : curRule.pattern === url) {
+                flag = true;
+                callback = curRule.callback;
+                break;
+            }
+        }
 
-		this.addEventListener('readystatechange', function(event) {
-			if (flag && this.readyState === 4) {
-				console.log('[ajax-intercept]', method, url);
-				var response = callback(event.target.responseText);
-				console.log('[ajax-intercept] response original', event.target.responseText, ', intercept result', response);
-				Object.defineProperty(this, 'response', { writable: true });
-				Object.defineProperty(this, 'responseText', { writable: true });
-				this.response = this.responseText = response;
-			}
-		});
+        this.addEventListener('readystatechange', function(event) {
+            if (flag && this.readyState === 4) {
+                console.log('[ajax-intercept]', method, url);
+                var response = callback(event.target.responseText);
+                console.log('[ajax-intercept] response original', event.target.responseText, ', intercept result', response);
+                Object.defineProperty(this, 'response', { writable: true });
+                Object.defineProperty(this, 'responseText', { writable: true });
+                this.response = this.responseText = response;
+            }
+        });
 
-		return ajax_open_original.apply(this, open_arguments);
-   	};
+        return ajax_open_original.apply(this, open_arguments);
+       };
 };
